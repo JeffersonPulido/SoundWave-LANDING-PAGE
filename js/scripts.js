ca9a80c058f7d73d2408f6c1e95e3a3af7fe5511
Jefferson Pulido <86859723+JeffersonPulido@users.noreply.github.com>
2022-07-12 11:06:07 -0500

    Formatos webp

--- js/scripts.js
+++ js/scripts.js
@@ -116,48 +116,6 @@ var cardSlider = new Swiper('.card-slider', {
 });
 
 
-/* Filter - Isotope */
-const gridCheck = document.querySelector('.grid');
-
-if (gridCheck !== null) { 
-	// init Isotope
-	var iso = new Isotope( '.grid', {
-		itemSelector: '.element-item',
-		layoutMode: 'fitRows'
-	});
-
-	// bind filter button click
-	var filtersElem = document.querySelector('.filters-button-group');
-	filtersElem.addEventListener( 'click', function( event ) {
-		// only work with buttons
-		if ( !matchesSelector( event.target, 'button' ) )  {
-			return;
-		}
-		var filterValue = event.target.getAttribute('data-filter');
-		// use matching filter function
-		iso.arrange({ filter: filterValue });
-	});
-	
-	// change is-checked class on buttons
-	var buttonGroups = document.querySelectorAll('.button-group');
-	for ( var i=0, len = buttonGroups.length; i < len; i++ ) {
-		var buttonGroup = buttonGroups[i];
-		radioButtonGroup( buttonGroup );
-	}
-	
-	function radioButtonGroup( buttonGroup ) {
-		buttonGroup.addEventListener( 'click', function( event ) {
-			// only work with buttons
-			if ( !matchesSelector( event.target, 'button' ) )  {
-				return;
-			}
-			buttonGroup.querySelector('.is-checked').classList.remove('is-checked');
-			event.target.classList.add('is-checked');
-		});
-	}
-}
-
-
 /* Back To Top Button */
 // Get the button
 myButton = document.getElementById("myBtn");
