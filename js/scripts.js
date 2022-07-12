f1ac941cf85be89d18ec726a948e5d008cf11a4d
Jefferson Pulido <86859723+JeffersonPulido@users.noreply.github.com>
2022-07-12 09:15:57 -0500

    Styles 2

--- js/scripts.js
+++ js/scripts.js
@@ -176,35 +176,3 @@ function topFunction() {
 	document.body.scrollTop = 0; // for Safari
 	document.documentElement.scrollTop = 0; // for Chrome, Firefox, IE and Opera
 }
-
-// WHatsApp PopUp
-popupWhatsApp = () => {
-  
-    let btnClosePopup = document.querySelector('.closePopup');
-    let btnOpenPopup = document.querySelector('.whatsapp-button');
-    let popup = document.querySelector('.popup-whatsapp');
-    let sendBtn = document.getElementById('send-btn');
-  
-    btnClosePopup.addEventListener("click",  () => {
-      popup.classList.toggle('is-active-whatsapp-popup')
-    })
-    
-    btnOpenPopup.addEventListener("click",  () => {
-      popup.classList.toggle('is-active-whatsapp-popup')
-       popup.style.animation = "fadeIn .16s 0.0s both";
-    })
-    
-    sendBtn.addEventListener("click", () => {
-    let msg = document.getElementById('whats-in').value;
-    let relmsg = msg.replace(/ /g,"%20");
-       
-     window.open('https://wa.me/573142475368?text='+relmsg, '_blank'); 
-    
-    });
-  
-    setTimeout(() => {
-      popup.classList.toggle('is-active-whatsapp-popup');
-    }, 50000);
-  }
-  
-  popupWhatsApp();
